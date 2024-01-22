4628975a6b155ce439784d8e54290c81f73087d6
lvlivingston <lvlivingston@gmail.com>
2024-01-21 22:20:17 -0600

    additional edits

--- script.py
+++ script.py
@@ -1,10 +1,21 @@
-# Exploring Selenium
+# Exploring Selenium with Upwork
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Access Upwork credentials from environment variables
+upwork_username = os.getenv("UPWORK_USERNAME")
+upwork_password = os.getenv("UPWORK_PASSWORD")
 
 # Set up the WebDriver (use the path to your ChromeDriver executable)
-driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
+driver = webdriver.Chrome(executable_path='../chromedriver-win64/chromedriver.exe')
 
 # Navigate to the Upwork page
 driver.get("https://www.upwork.com/nx/find-work/saved-jobs")
@@ -12,14 +23,27 @@ driver.get("https://www.upwork.com/nx/find-work/saved-jobs")
 # Wait for the page to load (increase wait time if needed)
 driver.implicitly_wait(2)
 
+# Log into Upwork credentials from .env file
+driver.find_element(By.NAME, "username").send_keys(upwork_username)
+driver.find_element(By.NAME, "password").send_keys(upwork_password)
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+# Wait for the login process to complete and a specific element to be present
+wait = WebDriverWait(driver, 5) # Adjust the timeout as needed
+
 # Find all h3 elements with the class 'job-tile-title'
 job_title_elements = driver.find_elements(by=By.XPATH, value="//h3[@class='job-tile-title']")
 
-# Extract and print text from each h3 element
-for element in job_title_elements:
-    text = element.text.strip()
-    if text:  # Check if the text is not empty
-        print(text)
+# Iterate through each job title element and click on it
+for job_title_element in job_title_elements:
+    # Click on the h3 element to open the full job posting
+    job_title_element.click()
+    # Wait for the page to load after clicking (increase wait time if needed)
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//div[@class='job-title']")))
+    # Perform actions on the opened job posting page, e.g., extract details
+    
+    # Go back to the main jobs page
+    driver.back()
 
 # Close the browser window
 driver.quit()
